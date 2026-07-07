08320c01581dde6c14c99e04aabb611a34b02356
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-06 19:58:07 -0700

    Initial Commit

---
+++ LemonRestaurantCh11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LemonRestaurantCh11
+//
+//  Created by Brittany Grigsby on 7/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "globe") // view 1
+                .imageScale(.large)
+                .foregroundStyle(Color.blue)
+            Text("Hello, World!")
+                
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LemonRestaurantCh11/LemonRestaurantCh11App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantCh11App.swift
+//  LemonRestaurantCh11
+//
+//  Created by Brittany Grigsby on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantCh11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
