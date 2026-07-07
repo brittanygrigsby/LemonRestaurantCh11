5047fc3007a87dbf064be511305f347130c4b287
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-06 20:00:55 -0700

    updating Text title

--- LemonRestaurantCh11/ContentView.swift
+++ LemonRestaurantCh11/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe") // view 1
                 .imageScale(.large)
                 .foregroundStyle(Color.blue)
-            Text("Hello, World!")
+            Text("Hello, World Coh 11!")
                 
         }
     }
